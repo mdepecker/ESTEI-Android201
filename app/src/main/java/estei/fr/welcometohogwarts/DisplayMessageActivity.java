9a106eefdb42ef0905c3664b98035779bbfd9573
mickael de pecker <mickael@cymoz.fr>
2015-09-18 00:22:22 +0200

    'expelliarmus'

---
+++ app/src/main/java/estei/fr/welcometohogwarts/DisplayMessageActivity.java
@@ -0,0 +1,15 @@
+package estei.fr.welcometohogwarts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mdepe on 17/09/2015.
+ */
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
